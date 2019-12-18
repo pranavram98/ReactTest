f4a81524cee2480d5e347f5f01988430965d6dff
Pranav R <pranav.r@C-LM-0326.local>
2019-12-18 16:08:07 +0530

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -24,7 +24,7 @@ class App extends React.Component{
     };
 
     componentDidMount(){
-        this.onTermSubmit('iOS');
+        this.onTermSubmit('MacBook');
     }
     render(){
         return(
